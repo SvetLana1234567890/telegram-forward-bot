2b46acc90a29c6cd2b4807de039233b82fb59fd1
SvetLana1234567890 <81438856+SvetLana1234567890@users.noreply.github.com>
2026-05-22 10:42:50 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,37 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+TARGET_CHAT_ID = int(os.getenv("TARGET_CHAT_ID"))
+
+
+async def forward_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    if not update.message:
+        return
+
+    # Не пересилати повідомлення з цільової групи
+    if update.effective_chat.id == TARGET_CHAT_ID:
+        return
+
+    source_chat = update.effective_chat.title
+    text = update.message.text or ""
+
+    message = f"🏪 Магазин: {source_chat}\n\n{text}"
+
+    await context.bot.send_message(
+        chat_id=TARGET_CHAT_ID,
+        text=message
+    )
+
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(
+    MessageHandler(filters.TEXT & ~filters.COMMAND, forward_message)
+)
+
+print("Bot started...")
+
+app.run_polling()
