987e76747fb2b3aeb366603da0eaa3d957f30525
SvetLana1234567890 <81438856+SvetLana1234567890@users.noreply.github.com>
2026-05-22 14:19:45 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,32 +1,13 @@
-from telegram import Update
-from telegram.ext import (
-    ApplicationBuilder,
-    MessageHandler,
-    CallbackQueryHandler,
-    filters,
-    ContextTypes
-)
+from telegram.ext import TypeHandler
 
-TOKEN = "8689968489:AAFr9p2oQuFo3e79JinPdk7FTAvspwUQL5E"
-TARGET_CHAT_ID = -5103853856
+async def forward_all(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
-
-async def forward_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-
-    msg = (
-        update.message
-        or update.edited_message
-        or update.channel_post
-    )
+    msg = update.message or update.channel_post or update.edited_message
 
     if not msg:
         return
 
-    text = msg.text or msg.caption or ""
-
-    # fallback для forwarded bot messages
-    if not text:
-        text = str(msg.to_dict())
+    text = msg.text or msg.caption or str(msg.to_dict())
 
     await context.bot.send_message(
         chat_id=TARGET_CHAT_ID,
@@ -36,8 +17,7 @@ async def forward_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 app = ApplicationBuilder().token(TOKEN).build()
 
-app.add_handler(MessageHandler(filters.ALL, forward_message))
+app.add_handler(TypeHandler(Update, forward_all))
 
-print("Bot started...")
 
-app.run_polling()
+
